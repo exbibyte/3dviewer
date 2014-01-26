5da16080517388dc834cf4e07e1ed9278b56ceb3
Bill Liu <adaptionist@gmail.com>
2014-01-26 12:38:25 -0800

    created file for model parser

---
+++ eece478/eece478/ModelParse.h
@@ -0,0 +1,18 @@
+#ifndef MODELPARSE_H
+#define MODELPARSE_H
+
+#include <string>
+
+using namespace std;
+
+class ModelParse
+{
+protected:
+
+public:
+	//load model and store model info within the class
+	ModelParse(string path);
+
+};
+
+#endif
