76b5579c06cd77209d5372f708492d45432b79e4
yl <yl@yl-ThinkPad-X220-Tablet.(none)>
2014-02-02 01:43:01 -0800

    completed and succeessfully tested initial version of matching vertex to triangle data

--- eece478/eece478/ModelEntity.cpp
+++ eece478/eece478/ModelEntity.cpp
@@ -1,5 +1,8 @@
 #include <tuple>
 #include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <vector>
 
 #include "ModelEntity.h"
 #include "ModelName.h"
@@ -7,6 +10,27 @@
 #include "ModelVertice.h"
 #include "ModelNormal.h"
 #include "ModelTriangle.h"
+
+
+bool ModelEntity::fSortTriangleDetailByVec1(const tTriangleDetail& A, const tTriangleDetail& B)
+{
+  return std::get<TTRIANGLEDETAIL_VEC1ID>(A) < std::get<TTRIANGLEDETAIL_VEC1ID>(B);
+}
+
+bool ModelEntity::fSortTriangleDetailByVec2(const tTriangleDetail& A, const tTriangleDetail& B)
+{
+  return std::get<TTRIANGLEDETAIL_VEC2ID>(A) < std::get<TTRIANGLEDETAIL_VEC2ID>(B);
+}
+
+bool ModelEntity::fSortTriangleDetailByVec3(const tTriangleDetail& A, const tTriangleDetail& B)
+{
+  return std::get<TTRIANGLEDETAIL_VEC3ID>(A) < std::get<TTRIANGLEDETAIL_VEC3ID>(B);
+}
+
+bool ModelEntity::fSortTriangleDetailByNorm(const tTriangleDetail& A, const tTriangleDetail& B)
+{
+  return std::get<TTRIANGLEDETAIL_NORMID>(A) < std::get<TTRIANGLEDETAIL_NORMID>(B);
+}
 
 void ModelEntity::GetVertices(float*& data, int& num)
 /**
@@ -19,7 +43,7 @@ num: array size
   num = this->vNumVertice*3;
 
 #ifdef DEBUG
-  cout<<"vertex count"<<num<<endl; 
+  cout<<"vertex data count"<<num<<endl; 
 #endif
 
   data = new float[num];
@@ -34,4 +58,122 @@ num: array size
     data++;
   }
   data -= num;
+}
+
+void ModelEntity::GetUpdatedVertices(float*& data, int& num)
+/**
+return:
+data: 1D array of vertice data
+num: array size
+*/
+{
+  this->vNumVertice = (this->vtTriangleDetail.size()*3);
+  num = this->vNumVertice*3;
+
+#ifdef DEBUG
+  cout<<"vertex data count"<<num<<endl; 
+#endif
+
+  data = new float[num];
+
+  for(auto i : this->vtTriangleDetail)
+  {
+    *data = std::get<TTRIANGLEDETAIL_VEC1X>(i);
+    data++;
+    *data = std::get<TTRIANGLEDETAIL_VEC1Y>(i);
+    data++;
+    *data = std::get<TTRIANGLEDETAIL_VEC1Z>(i);
+    data++;
+    *data = std::get<TTRIANGLEDETAIL_VEC2X>(i);
+    data++;
+    *data = std::get<TTRIANGLEDETAIL_VEC2Y>(i);
+    data++;
+    *data = std::get<TTRIANGLEDETAIL_VEC2Z>(i);
+    data++;
+    *data = std::get<TTRIANGLEDETAIL_VEC3X>(i);
+    data++;
+    *data = std::get<TTRIANGLEDETAIL_VEC3Y>(i);
+    data++;
+    *data = std::get<TTRIANGLEDETAIL_VEC3Z>(i);
+    data++;
+  }
+  data -= num;
+}
+
+void ModelEntity::Update()
+{ 
+  this->vtTriangleDetail.clear();
+  
+  //copy IDs
+  for(auto i : cModelTriangle->vTriangle)
+  {
+    tTriangleDetail detail;
+    std::get<TTRIANGLEDETAIL_ID>(detail) = std::get<TTRIANGLE_ID>(i);
+    std::get<TTRIANGLEDETAIL_VEC1ID>(detail) = std::get<TTRIANGLE_VEC1ID>(i);
+    std::get<TTRIANGLEDETAIL_VEC2ID>(detail) = std::get<TTRIANGLE_VEC2ID>(i);
+    std::get<TTRIANGLEDETAIL_VEC3ID>(detail) = std::get<TTRIANGLE_VEC3ID>(i);
+    std::get<TTRIANGLEDETAIL_NORMID>(detail) = std::get<TTRIANGLE_NORMID>(i);
+    this->vtTriangleDetail.push_back(detail);
+  }
+
+  //sort triangles by 1st vertex
+  std::sort(this->vtTriangleDetail.begin(), this->vtTriangleDetail.end(), fSortTriangleDetailByVec1); 
+  //assign values when IDs match
+  vector<tVertice>::iterator itVertice = this->cModelVertice->vVertice.begin(); 
+  vector<tTriangleDetail>::iterator itDetail = this->vtTriangleDetail.begin(); 
+  while(itVertice != this->cModelVertice->vVertice.end() && itDetail != this->vtTriangleDetail.end())   
+  {
+    if(std::get<TVERTICE_ID>(*itVertice) == std::get<TTRIANGLEDETAIL_VEC1ID>(*itDetail))
+    {
+      std::get<TTRIANGLEDETAIL_VEC1X>(*itDetail) = std::get<TVERTICE_X>(*itVertice);
+      std::get<TTRIANGLEDETAIL_VEC1Y>(*itDetail) = std::get<TVERTICE_Y>(*itVertice);
+      std::get<TTRIANGLEDETAIL_VEC1Z>(*itDetail) = std::get<TVERTICE_Z>(*itVertice);
+      itDetail++;
+    }
+    else
+    {
+      itVertice++;
+    }
+  }
+
+  //sort triangles by 2nd vertex
+  std::sort(this->vtTriangleDetail.begin(), this->vtTriangleDetail.end(), fSortTriangleDetailByVec2); 
+  //assign values when IDs match
+  itVertice = this->cModelVertice->vVertice.begin(); 
+  itDetail = this->vtTriangleDetail.begin(); 
+  while(itVertice != this->cModelVertice->vVertice.end() && itDetail != this->vtTriangleDetail.end())   
+  {
+    if(std::get<TVERTICE_ID>(*itVertice) == std::get<TTRIANGLEDETAIL_VEC2ID>(*itDetail))
+    {
+      std::get<TTRIANGLEDETAIL_VEC2X>(*itDetail) = std::get<TVERTICE_X>(*itVertice);
+      std::get<TTRIANGLEDETAIL_VEC2Y>(*itDetail) = std::get<TVERTICE_Y>(*itVertice);
+      std::get<TTRIANGLEDETAIL_VEC2Z>(*itDetail) = std::get<TVERTICE_Z>(*itVertice);
+      itDetail++;
+    }
+    else
+    {
+      itVertice++;
+    }
+  }
+
+  //sort triangles by 3rd vertex
+  std::sort(this->vtTriangleDetail.begin(), this->vtTriangleDetail.end(), fSortTriangleDetailByVec3); 
+  //assign values when IDs match
+  itVertice = this->cModelVertice->vVertice.begin(); 
+  itDetail = this->vtTriangleDetail.begin(); 
+  while(itVertice != this->cModelVertice->vVertice.end() && itDetail != this->vtTriangleDetail.end())   
+  {
+    if(std::get<TVERTICE_ID>(*itVertice) == std::get<TTRIANGLEDETAIL_VEC3ID>(*itDetail))
+    {
+      std::get<TTRIANGLEDETAIL_VEC3X>(*itDetail) = std::get<TVERTICE_X>(*itVertice);
+      std::get<TTRIANGLEDETAIL_VEC3Y>(*itDetail) = std::get<TVERTICE_Y>(*itVertice);
+      std::get<TTRIANGLEDETAIL_VEC3Z>(*itDetail) = std::get<TVERTICE_Z>(*itVertice);
+      cout<<"vec3 z: "<<std::get<TTRIANGLEDETAIL_VEC3X>(*itDetail)<<endl;
+      itDetail++;
+    }
+    else
+    {
+      itVertice++;
+    }
+  }
 }
--- eece478/eece478/ModelEntity.h
+++ eece478/eece478/ModelEntity.h
@@ -1,13 +1,42 @@
 #ifndef MODEL_ENTITY_H
 #define MODEL_ENTITY_H
 
+#include <vector>
+#include <tuple>
+
 #include "ModelName.h"
 #include "ModelTexture.h"
 #include "ModelVertice.h"
 #include "ModelNormal.h"
 #include "ModelTriangle.h"
 
+#define TTRIANGLEDETAIL_ID 0
+#define TTRIANGLEDETAIL_VEC1ID 1
+#define TTRIANGLEDETAIL_VEC2ID 2
+#define TTRIANGLEDETAIL_VEC3ID 3
+#define TTRIANGLEDETAIL_NORMID 4
+#define TTRIANGLEDETAIL_VEC1X 5
+#define TTRIANGLEDETAIL_VEC1Y 6
+#define TTRIANGLEDETAIL_VEC1Z 7
+#define TTRIANGLEDETAIL_VEC2X 8
+#define TTRIANGLEDETAIL_VEC2Y 9
+#define TTRIANGLEDETAIL_VEC2Z 10
+#define TTRIANGLEDETAIL_VEC3X 11
+#define TTRIANGLEDETAIL_VEC3Y 12
+#define TTRIANGLEDETAIL_VEC3Z 13
+#define TTRIANGLEDETAIL_NORMX 14
+#define TTRIANGLEDETAIL_NORMY 15
+#define TTRIANGLEDETAIL_NORMZ 16
+
+typedef tuple<int, int,int,int, int, float,float,float,float,float,float,float,float,float, float,float,float> tTriangleDetail;
+
 class ModelEntity{
+private:
+  static bool fSortTriangleDetailByVec1(const tTriangleDetail&, const tTriangleDetail&);
+  static bool fSortTriangleDetailByVec2(const tTriangleDetail&, const tTriangleDetail&);
+  static bool fSortTriangleDetailByVec3(const tTriangleDetail&, const tTriangleDetail&);
+  static bool fSortTriangleDetailByNorm(const tTriangleDetail&, const tTriangleDetail&);
+  
 public:
   int vNumVertice;
   ModelName * cModelName;
@@ -15,7 +44,12 @@ public:
   ModelVertice * cModelVertice;
   ModelNormal * cModelNormal;
   ModelTriangle * cModelTriangle;
+  
+  vector<tTriangleDetail> vtTriangleDetail;
+  
   void GetVertices(float*& data, int& num);
+  void GetUpdatedVertices(float*& data, int& num);
+  void Update();
 };
 
 #endif
