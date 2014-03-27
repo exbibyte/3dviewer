c8ee3938cad6e5c536f5412ce6a38eb1dfd1b8f4
Bill Liu <yuan6liu@gmail.com>
2014-03-26 23:22:08 -0700

    slight edit on curve test file

--- eece478/test/CurveParse.cpp
+++ eece478/test/CurveParse.cpp
@@ -13,13 +13,25 @@ int main(int argc, char** argv)
   TrajectoryParse parser;
   
   vector<CurvePath *> vCurve = parser.GetTrajectories(argv[1]);
-  
+
+  if(vCurve.empty())
+    return -1;
+
   cout<<"number of curves: "<<vCurve.size()<<endl;
 
   cout<<"current position: "<<endl;
+
+  int curvecount = 0;
+
   for(auto i : vCurve)
   {
-    i->Increment();
-    i->PrintPosition();
+    cout<<"curve number: "<<curvecount<<endl;
+    for(int j = 0; j < 45; j++)
+    {
+      i->Increment();
+      i->PrintPosition();
+    }
+    curvecount++;
+    break;
   }
 }
