a8291f30f325c817e56e91a6698a8db088f07c4f
Bill Liu <yuan6liu@gmail.com>
2014-03-27 20:26:01 -0700

    added ModelPool class

---
+++ eece478/src/ModelPool.h
@@ -0,0 +1,24 @@
+#ifndef MODELPOOL_H
+#define MODELPOOL_H
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class ModelAbstraction;
+
+/// provides storage of world objects
+class ModelPool
+{
+ private:
+  vector<ModelAbstraction*> * pvpModels;
+ public:
+  ModelPool();
+  ModelAbstraction * GetModel(string name);
+  bool AddModel(ModelAbstraction * model);
+  bool RemoveModel(ModelAbstraction * model);
+  bool RemoveModel(string name);
+  void SetModelSource(vector<ModelAbstraction*> *);
+};
+#endif
