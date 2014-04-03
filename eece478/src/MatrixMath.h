39a425b6f2856ccd80387538d4f4feb5069a496d
Bill Liu <yuan6liu@gmail.com>
2014-04-02 20:00:13 -0700

    added header comments

--- eece478/src/MatrixMath.h
+++ eece478/src/MatrixMath.h
@@ -1,10 +1,10 @@
-// from http://stackoverflow.com/questions/1148309/inverting-a-4x4-matrix
-
 #ifndef MATRIXMATH_H
 #define MATRIXMATH_H
 
+///methods for matrix arithmetic in column major format
 namespace MatrixMath{
   /// provides matrix inversion
+  /// from http://stackoverflow.com/questions/1148309/inverting-a-4x4-matrix
   bool InvertMatrix(const float m[16], float invOut[16]);
 
   /// provides 4x1 * 4x4 matrix operation 
@@ -13,6 +13,7 @@ namespace MatrixMath{
   /// provides 4x4 * 4x4 matrix operation 
   void Mat4x4Mult4x4(float Left[], float Right[], float out[]);
 
+  /// transposes 4x4 matrix
   void Mat4x4Transpose(float in[], float out[]);
 }
 #endif
