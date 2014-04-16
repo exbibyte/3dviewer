6177ab5e58c45ad5c115e4f9760c1369ac083dfe
Bill Liu <yuan6liu@gmail.com>
2014-04-15 17:28:38 -0700

    changed from bilinear to trilinear for minification filter

--- eece478/src/ModelEntity.cpp
+++ eece478/src/ModelEntity.cpp
@@ -653,10 +653,13 @@ Load texture image files and bind to OpenGL
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 
     //pass texture to openGL
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, ImgData);
+
+    //let opengl automatically build mipmap
+    glGenerateMipmap(GL_TEXTURE_2D);
     
     //delte image data
     delete [] ImgData;
